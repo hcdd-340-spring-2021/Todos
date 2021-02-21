885edfe258d06cca57ef76206365e4f1f6bb54fc
Saeed Abdullah <me@saeedabdullah.com>
2021-02-21 17:58:52 -0500

    Adds and deletes todos

--- App.js
+++ App.js
@@ -1,13 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  Button,
+  View,
+  TextInput,
+  Alert,
+  SafeAreaView,
+  FlatList
+} from 'react-native';
+import ToDo from './ToDo.js'
 
 export default function App() {
+  // Here's what we will do step by step
+  // 1. Add the text value in the textInput to the list todos which is in our state
+  // 2. Render the FlatList!
+  // 3. Make Todo items deletable by clicking on them
+
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    const todosCopy = JSON.parse(JSON.stringify(todos));
+    todosCopy.push(text);
+    setTodos(todosCopy);
+    setText("");
+  }
+
+  // 3.1 Add a function to delete an item from todos given its
+  // index
+  const deleteTodo = index => {
+    const todosCopy = JSON.parse(JSON.stringify(todos));
+    todosCopy.splice(index, 1);
+    setTodos(todosCopy);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.flatlist}>
+        {/*
+
+          // 2. Here, you will render your FlatList. To help you do
+          // that, we have created a component for you to render the
+          // items. Additionally, here are the props you will pass to
+          // FlatList:
+          // - style: Make sure your list takes all the white space
+          // - data: What is the list of items we are trying to render?
+          // - renderItem: Use the included ToDo to render items
+          // ToDo component is called like this <ToDo text={"Hello"}/>
+          // - keyExtractor: Write a one-line function to take
+          // (item, index) in and returns index.toString()
+
+        */}
+          <FlatList
+            data={todos}
+            renderItem={( { item, index } ) =>
+              <ToDo
+                id={index}
+                text={item}
+                onSelect={deleteTodo}
+                />
+             }
+             keyExtractor={(item, index) => {
+               return item + index.toString()
+            }}
+          />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <TextInput
+            style={styles.textinput}
+            onChangeText={text => setText(text)} /*What method should be called here? */
+            value={text} /*What should be in place of the empty string? */
+          />
+          <Button
+            style={styles.button}
+            title="Add"
+            onPress={addTodo} /*What should be called here? */
+          />
+        </View>
+
+      </SafeAreaView>
   );
 }
 
@@ -18,4 +90,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  flatlist: {
+    flex: 1
+  },
+  textinput: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  button: {
+    height: 40,
+    width: '20%',
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
